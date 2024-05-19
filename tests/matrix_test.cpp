17a6b64c2c90f6bd85fc468a482464e4dab5f1f5
ivan <ivantey228@gmail.com>
2024-05-19 15:07:02 +0300

    transpose test

--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -213,7 +213,13 @@ TEST(MatrixTest, Transpose) {
     matrix(2, 1) = 8;
     matrix(2, 2) = 9;
 
+    Matrix matrix2 = {
+            {1, 2, 3},
+            {4, 5, 6}
+    };
+
     Matrix transposedMatrix = matrix.transpose();
+    Matrix transposedMatrix2 = matrix2.transpose();
 
     EXPECT_EQ(transposedMatrix(0, 0), 1);
     EXPECT_EQ(transposedMatrix(0, 1), 4);
@@ -224,6 +230,13 @@ TEST(MatrixTest, Transpose) {
     EXPECT_EQ(transposedMatrix(2, 0), 3);
     EXPECT_EQ(transposedMatrix(2, 1), 6);
     EXPECT_EQ(transposedMatrix(2, 2), 9);
+
+    EXPECT_EQ(transposedMatrix(0, 0), 1);
+    EXPECT_EQ(transposedMatrix(0, 1), 4);
+    EXPECT_EQ(transposedMatrix(1, 0), 2);
+    EXPECT_EQ(transposedMatrix(1, 1), 5);
+    EXPECT_EQ(transposedMatrix(2, 0), 3);
+    EXPECT_EQ(transposedMatrix(2, 1), 6);
 }
 
 // Тестирование вычисления определителя матрицы
@@ -265,31 +278,92 @@ TEST(MatrixTest, CreateIdentity) {
     EXPECT_EQ(identityMatrix(2, 2), 1);
 }
 
-// Тестирование решения системы линейных уравнений // TODO: Починка
-//TEST(MatrixTest, Solve) {
-//    Matrix A(3, 3);
-//    Matrix b(3, 1);
-//
-//    A(0, 0) = 3;
-//    A(0, 1) = 2;
-//    A(0, 2) = -1;
-//    A(1, 0) = 2;
-//    A(1, 1) = -2;
-//    A(1, 2) = 4;
-//    A(2, 0) = -1;
-//    A(2, 1) = 0.5;
-//    A(2, 2) = -1;
-//
-//    b(0, 0) = -3;
-//    b(1, 0) = -2;
-//    b(2, 0) = -1;
-//
-//    Matrix x = Matrix::solve(A, b);
-//
-//    EXPECT_FLOAT_EQ(x(0, 0), 1);
-//    EXPECT_FLOAT_EQ(x(1, 0), -2);
-//    EXPECT_FLOAT_EQ(x(2, 0), -1);
-//}
+// Тестирование решения системы линейных уравнений
+TEST(MatrixTest, Solve3x3) {
+    Matrix A = {
+            {1, 2, 3},
+            {3, 5, 7},
+            {1, 3, 4}
+    };
+
+    Matrix B = {
+            {3},
+            {0},
+            {1}
+    };
+
+    Matrix x = Matrix::solve(A, B);
+
+    EXPECT_FLOAT_EQ(x(0, 0), -4);
+    EXPECT_FLOAT_EQ(x(1, 0), -13);
+    EXPECT_FLOAT_EQ(x(2, 0), 11);
+}
+
+TEST(MatrixTest, Solve4x4) {
+    Matrix A1 = {
+            {1, 2, 2, -3},
+            {2, -1, 1, 2},
+            {2, 4, -3, 5},
+            {2, 1 , 2, -1}
+    };
+
+    Matrix B1 = {
+            {-2},
+            {-1},
+            {4},
+            {-2}
+    };
+
+    Matrix A2 = {
+            {1, 2, 3, 4},
+            {-1, 2, -3, 4},
+            {1, 1, -1, 1},
+            {1, 1 , 1, 1}
+    };
+
+    Matrix B2 = {
+            {30},
+            {10},
+            {3},
+            {10}
+    };
+
+    Matrix A3 = {
+            {3, 1, -2, -22},
+            {2, -1, 2, 2},
+            {2, 1, -1, -1},
+            {1, 1 , -3, 2}
+    };
+
+    Matrix B3 = {
+            {-2},
+            {2},
+            {-1},
+            {-3}
+    };
+
+    Matrix res1 = Matrix::solve(A1, B1);
+    Matrix res2 = Matrix::solve(A2, B2);
+    Matrix res3 = Matrix::solve(A3, B3);
+
+    // res1
+    EXPECT_FLOAT_EQ(res1(0, 0), -0.25);
+    EXPECT_FLOAT_EQ(res1(1, 0), 0.25);
+    EXPECT_FLOAT_EQ(res1(2, 0), -0.75);
+    EXPECT_FLOAT_EQ(res1(3, 0), 0.25);
+
+    // res2
+    EXPECT_FLOAT_EQ(res2(0, 0), -0.5);
+    EXPECT_FLOAT_EQ(res2(1, 0), 4);
+    EXPECT_FLOAT_EQ(res2(2, 0), 3.5);
+    EXPECT_FLOAT_EQ(res2(3, 0), 3);
+
+    // res3
+    EXPECT_FLOAT_EQ(res3(0, 0), 0);
+    EXPECT_FLOAT_EQ(res3(1, 0), 0);
+    EXPECT_FLOAT_EQ(res3(2, 0), 1);
+    EXPECT_FLOAT_EQ(res3(3, 0), 0);
+}
 
 // Тестирование скалярного произведения векторов
 TEST(MatrixTest, DotProduct) {
@@ -377,41 +451,6 @@ TEST(MatrixTest, GaussianEliminate) {
     EXPECT_FLOAT_EQ(rowEchelonForm(2, 2), 0);
     EXPECT_FLOAT_EQ(rowEchelonForm(2, 3), 0);
 }
-
-// RowReduceFromGaussian Тестирование сокращения матрицы до редуцированного ступенчатого вида
-/*
-TEST(MatrixTest, RowReduceFromGaussian) {
-    Matrix A(3, 4);
-
-    A(0, 0) = 1;
-    A(0, 1) = 2;
-    A(0, 2) = 3;
-    A(0, 3) = 4;
-    A(1, 0) = 4;
-    A(1, 1) = 5;
-    A(1, 2) = 6;
-    A(1, 3) = 7;
-    A(2, 0) = 7;
-    A(2, 1) = 8;
-    A(2, 2) = 9;
-    A(2, 3) = 10;
-
-    Matrix rowReducedEchelonForm = A.gaussianEliminate().rowReduceFromGaussian();
-
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(0, 0), 1);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(0, 1), 2);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(0, 2), 3);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(0, 3), 4);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(1, 0), 0);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(1, 1), 1);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(1, 2), 2);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(1, 3), 3);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(2, 0), 0);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(2, 1), 0);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(2, 2), 0);
-    EXPECT_FLOAT_EQ(rowReducedEchelonForm(2, 3), 0);
-}
-*/
 
 // Тестирование нахождения обратной матрицы
 TEST(MatrixTest, Inverse) {
