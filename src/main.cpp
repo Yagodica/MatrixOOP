a65f1e8febf5b006a5296be6cd4254b149df7898
ivan <ivantey228@gmail.com>
2024-05-20 15:04:38 +0300

    увг

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,26 @@
 using namespace std;
 
 int main() {
-    try {
-        // Считывание изображения из PGM-файла
-        Image image(5.0, 5, "input.pgm");
+    Matrix a(2, 2);
 
-        // Примените к изображению размытие по Гауссу
-        image.applyGaussianBlur();
+    cout << a;
 
-        // Сохранение результата в новом PGM-файле
-        image.saveToPGMFile("output_test.pgm");
-
-        cout << "Размытие по Гауссу успешно применено к изображению и сохранено в файл." << endl;
-    }
-    catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
-        return 1;
-    }
+//    try {
+//        // Считывание изображения из PGM-файла
+//        Image image(5.0, 5, "input.pgm");
+//
+//        // Примените к изображению размытие по Гауссу
+//        image.applyGaussianBlur();
+//
+//        // Сохранение результата в новом PGM-файле
+//        image.saveToPGMFile("output_test.pgm");
+//
+//        cout << "Размытие по Гауссу успешно применено к изображению и сохранено в файл." << endl;
+//    }
+//    catch (const exception& e) {
+//        cerr << "Error: " << e.what() << endl;
+//        return 1;
+//    }
 
     return 0;
 }
